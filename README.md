63f5d73876e9b12a3d6eb09e809d17ee634ab12f
David Négrier <david@davidtcm>
2012-09-08 17:05:10 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,20 @@
+What is this package?
+=====================
+
+This package contains a basic renderer to render in HTML a menu (using &lt;ul&gt; and &lt;li&gt; tags).
+The renderer is making extensive use of objects declared in the (mouf\html.widgets.menu)[https://github.com/thecodingmachine/html.widgets.menu] package).
+base objects and interfaces to describe menus and menu items.
+Basically, using interfaces and objects of this package, you can describe a menu.
+
+Mouf package
+------------
+
+This package is part of Mouf (http://mouf-php.com), an effort to ensure good developing practices by providing a graphical dependency injection framework.
+Using Mouf's user interface, you can create your menu graphically, by creating instances of Menu and MenuItem.
+
+In practice
+-----------
+
+A menu is defined using the Menu class.
+The Menu class can contain many MenuItem. Each menuitem can contain many MenuItem.
+You pass a Menu instance to the BasicMenuRenderer::toHtml and it will render the menu. 
---
+++ src/Mouf/Html/Template/Menus/BasicMenuRenderer.php
@@ -0,0 +1,95 @@
+<?php
+/*
+ * Copyright (c) 2012 David Negrier
+ * 
+ * See the file LICENSE.txt for copying permission.
+ */
+
+
+/**
+ * This class is in charge of rendering a menu. It contains a menu and can transform it in HTML using the toHtml() method.
+ * <p>The rendering is performed using &lt;ul&gt; and &lt;li&gt; tags.</p>
+ * 
+ * @Component
+ */
+class BasicMenuRenderer implements HtmlElementInterface {
+	
+	/**
+	 * The menu to render
+	 *
+	 * @Property
+	 * @Compulsory
+	 * @var MenuInterface
+	 */
+	public $menu;
+	
+	/**
+	 * The whole CSS class to apply to the UL element.
+	 * 
+	 * @Property
+	 * @Compulsory
+	 * @var string
+	 */
+	public $cssClass = "menu";
+	
+	/**
+	 * Initialize the object, optionnally with the array of menu items to be displayed.
+	 *
+	 * @param array<SplashMenuItem> $menuItems
+	 */
+	public function __construct($menu = null) {
+		$this->menu = $menu;
+	}
+	
+	public function toHtml() {
+		if ($this->menu && !$this->menu->isHidden()) {
+			echo '<ul class="'.$this->cssClass.'">';
+			
+			$menuItems = $this->menu->getChildren();
+			if (is_array($menuItems)) {
+				foreach ($menuItems as $item) {
+					$this->renderHtmlMenuItem($item);
+				}
+			}
+				
+			echo '</ul>';
+		}
+	}
+	
+	private function renderHtmlMenuItem(MenuItemInterface $menuItem) {
+		if (!$menuItem->isHidden()) {
+			echo '<li ';
+			$menuCssClass = $menuItem->getCssClass();
+			if (!empty($menuCssClass) || $menuItem->isActive() || $menuItem->isExtended()) {
+				echo 'class="';
+				echo $menuCssClass;
+				if ($menuItem->isActive()) {
+					echo ' active';
+				}
+				echo '"';
+			}
+			echo '>';
+			$url = $menuItem->getLink();
+			if ($url) {
+				echo '<a href="'.$url.'" >';
+			}
+			echo $menuItem->getLabel();
+			if ($url) {
+				echo '</a>';
+			}
+			$children = $menuItem->getChildren();
+			if ($children) {
+				echo '<ul>';
+				foreach ($children as $child) {
+					/* @var $child MenuItemInterface */
+					$this->renderHtmlMenuItem($child);
+				}
+				echo '</ul>';
+			}
+			
+			echo '</li>';
+		}
+	}
+	
+}
+?>
